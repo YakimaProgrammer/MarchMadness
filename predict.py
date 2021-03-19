635f08252526180ced6f3c831f907b4e969e1024
Magnus Fulton <magnusjackfulton@gmail.com>
2021-03-19 15:56:37 -0700

    Added a couple of comments to predict.py so there is less confusion

--- predict.py
+++ predict.py
@@ -1,1 +1,3 @@
 from builddataset import X_train, X_test, y_train, y_test, inverse_scale
+#Want to see a model in action? Check out another branch in this repository!
+#Ready to make your own model? Start coding down below!
