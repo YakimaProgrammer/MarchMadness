803ff401e3215f7ddc39c741e3843da73b2236d5
Magnus Fulton <magnusjackfulton@gmail.com>
2021-03-22 10:35:47 -0700

    Paved the way for classifier and regressor support with the 'build_train_test_split' function

--- predict.py
+++ predict.py
@@ -1,3 +1,10 @@
-from builddataset import X_train, X_test, y_train, y_test, inverse_scale
+from builddataset import build_train_test_split, inverse_scale
+#regressor
+#X_train, X_test, y_train, y_test = build_train_test_split(["home_points","away_points","home_won"], ["home_points","away_points"])
+
+#classifier
+#X_train, X_test, y_train, y_test = build_train_test_split(["home_points","away_points","home_won"], ["home_won"])
+
+
 #Want to see a model in action? Check out another branch in this repository!
 #Ready to make your own model? Start coding down below!
